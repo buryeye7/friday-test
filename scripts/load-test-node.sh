229ddc9c8ddd43c795a9f97f014b95b87e698f25
Oracle Public Cloud User <opc@node-2.subnet.vcn.oraclevcn.com>
2020-04-17 05:26:53 +0000

    1

--- scripts/load-test-node.sh
+++ scripts/load-test-node.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-cat ~/git/friday-test/settings/account-address.txt | while read line
+PW=12345678
+for i in {1..10000}
 do
-  	clif hdac transfer-to $line 1 0.01 30000000  --from $line
+	for j in {1..100}
+	do
+		echo $j
+		res=$(expect -c "
+		spawn clif hdac transfer-to friday1sqv4r0lw2t2fqdq5j3xfrt6lpktl35dlte8st0 1 0.01 30000000 --from node$j --chain-id testnet
+		expect "N]:"
+			send \"y\\r\"  
+		expect "\'node$j\':"
+			send \"$PW\\r\"
+		expect eof 
+		" | sed '/confirm/d' | sed '/Password/d' ) 
+		echo "$res" | grep "txhash:"
+		echo "$res" | grep "code:"
+	done
 done
