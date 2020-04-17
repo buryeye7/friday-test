99c1799797479b8cc29b97d7d880129de300febc
Oracle Public Cloud User <opc@node-1.subnet.vcn.oraclevcn.com>
2020-04-17 05:26:13 +0000

    1

--- scripts/load-test.sh
+++ scripts/load-test.sh
@@ -7,7 +7,7 @@ do
 	do
 		echo $j
 		res=$(expect -c "
-		spawn clif hdac transfer-to friday1sqv4r0lw2t2fqdq5j3xfrt6lpktl35dlte8st0 1 0.01 30000000 --from node$j
+		spawn clif hdac transfer-to friday1sqv4r0lw2t2fqdq5j3xfrt6lpktl35dlte8st0 1 0.01 30000000 --from node$j --chain-id testnet
 		expect "N]:"
 			send \"y\\r\"  
 		expect "\'node$j\':"
