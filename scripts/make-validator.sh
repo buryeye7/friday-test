9d1da21d90f6f9dedd0ab5cea874e6d03336ba7b
Oracle Public Cloud User <opc@node-2.subnet.vcn.oraclevcn.com>
2020-04-17 04:37:16 +0000

    bf

---
+++ scripts/make-validator.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# == 0 ];then
+	echo "Input node name"
+	exit 0
+fi
+
+CN="testnet"
+clif hdac create-validator \
+--from $1 \
+--pubkey $(nodef tendermint show-validator) \
+--moniker node2 \
+--chain-id $CN
+
+clif hdac bond \
+--from $1 1 0.01 30000000 \
+--chain-id $CN 
