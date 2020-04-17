65bef1b45c98accd9394ba4cd11f1a996425abb5
Oracle Public Cloud User <opc@node-1.subnet.vcn.oraclevcn.com>
2020-04-17 04:19:39 +0000

    add files

---
+++ scripts/give-money.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ $# == 0 ];then
+	echo "Please node name"
+	exit 0
+fi 
+
+SRC_PATH="$HOME/git/friday-test/settings/$1-address.txt"
+
+PW="12345678"
+i=1
+amount=100000000
+cat $SRC_PATH | while read line 
+do
+	echo "i:" $i
+	echo "address:" $line
+	expect -c "
+        spawn clif hdac transfer-to $line 100000000 0.01 30000000 --from node$i
+        expect "N]:"
+            send \"y\\r\"
+        expect "\'node$i\':"
+            send \"$PW\\r\"
+        expect eof
+        "
+	i=$((i+1))
+done
+
+
+
