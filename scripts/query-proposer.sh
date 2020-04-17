fced8d56c6e8ba3cc0492ee79897f40bc844bece
Oracle Public Cloud User <opc@node-1.subnet.vcn.oraclevcn.com>
2020-04-17 07:28:54 +0000

    1

--- scripts/query-proposer.sh
+++ scripts/query-proposer.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-for i in {3000..3092}
+if [ $# != 2 ];then
+	echo "input start end"
+	exit 0
+fi 
+
+for i in {$1..$2}
 do
 	clif query block $i | jq .block.header.proposer_address
 done
