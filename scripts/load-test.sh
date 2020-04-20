ed2ef7655563a244b22163ee61e9eda5a204cbed
Jaehyun Lee <buryeye7@naver.com>
2020-04-20 00:55:59 +0000

    scripts/load-test.sh

--- scripts/load-test.sh
+++ scripts/load-test.sh
@@ -6,8 +6,10 @@ do
 	for j in {1..100}
 	do
 		echo $j
+		random=$(expr $RANDOM % 100)
+		random=$((random+1))
 		res=$(expect -c "
-		spawn clif hdac transfer-to friday1sqv4r0lw2t2fqdq5j3xfrt6lpktl35dlte8st0 1 0.01 30000000 --from node$j --chain-id testnet
+		spawn clif hdac transfer-to friday1sqv4r0lw2t2fqdq5j3xfrt6lpktl35dlte8st0 $random 0.01 30000000 --from node$j --chain-id testnet
 		expect "N]:"
 			send \"y\\r\"  
 		expect "\'node$j\':"
@@ -17,6 +19,7 @@ do
 		echo $res
 		txhash=$(echo $res | jq .txhash | sed 's/null//g')
 		code=$(echo $res | jq .code | sed 's/null//g')
-		echo $txhash $code
+		echo "txhash:" $txhash 
+		echo "code: " $code
 	done
 done
