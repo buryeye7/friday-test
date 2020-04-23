c0033a0647cfa049098a4f080047fa49b7f01e78
Oracle Public Cloud User <opc@node-5.subnet.vcn.oraclevcn.com>
2020-04-23 04:32:41 +0000

    tendermint

---
+++ scripts/tendermint-send-tx.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+rm diff.txt
+i=10000000000
+count=10000
+end=$((i + count))
+while [ $i -lt $end ]
+do
+	echo $i
+	i=$((i+1))
+	start_time=$(echo '('`date +"%s.%N"` ' * 1000000)/1' | bc)
+	curl -s "localhost:26657/broadcast_tx_async?tx=\"name=satoshi$i\""
+	end_time=$(echo '('`date +"%s.%N"` ' * 1000000)/1' | bc)
+	diff=$((end_time - start_time))
+	echo $diff >> diff.txt
+done
+
+sum=0
+cat diff.txt | while read line
+do
+	echo "line:" $line
+	sum=`expr $sum + $line`
+	echo "sum:" $sum
+done
