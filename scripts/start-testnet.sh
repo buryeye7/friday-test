49087a73f36d7792efdd5f5a03694e9004fef203
Oracle Public Cloud User <opc@instance-20200423-1856.subnet.vcn.oraclevcn.com>
2020-04-23 11:35:29 +0000

    .

--- scripts/start-testnet.sh
+++ scripts/start-testnet.sh
@@ -15,9 +15,19 @@ do
 done 
 
 rm -rf mytestnet
-tendermint testnet
 
-sed -i "s/prometheus = false/prometheus = true/g" ./mytestnet/node0/config/config.toml
+if [ $1 == 0 ];then
+	tendermint testnet
+	git add mytestnet
+	git commit -m "."
+	git push origin master
+fi
+
+for i in {1..4}
+do
+	tmp=$((i-1))
+	sed -i "s/prometheus = false/prometheus = true/g" ./mytestnet/node$tmp/config/config.toml
+done
 
 ID0=$(tendermint show_node_id --home ./mytestnet/node0)
 ID1=$(tendermint show_node_id --home ./mytestnet/node1)
@@ -27,4 +37,4 @@ IP0="10.0.0.39"
 IP1="10.0.0.40"
 IP2="10.0.0.41"
 
-tendermint node --home ./mytestnet/node$1 --proxy_app=kvstore --p2p.persistent_peers=&quot;ID0@IP0:26656,ID1@IP1:26656,ID2@IP2:26656&quot;
+tendermint node --home ./mytestnet/node$1 --proxy_app=kvstore --p2p.persistent_peers=$ID0@$IP0:26656,$ID1@$IP1:26656,$ID2@$IP2:26656
