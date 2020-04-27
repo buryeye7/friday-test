12e55e3e5f1ab6b7b23803ffeb63dc47b655f7dd
Oracle Public Cloud User <opc@instance-20200423-1856.subnet.vcn.oraclevcn.com>
2020-04-27 03:49:38 +0000

    .

--- scripts/start-testnet-gd.sh
+++ scripts/start-testnet-gd.sh
@@ -34,9 +34,9 @@ ID1=$(tendermint show_node_id --home ./mytestnet/node1)
 ID2=$(tendermint show_node_id --home ./mytestnet/node2)
 ID3=$(tendermint show_node_id --home ./mytestnet/node3)
 
-IP0="132.145.95.26"
-IP1="140.238.17.132"
-IP2="150.136.254.253"
-IP3="158.101.114.4"
+IP0="132.145.82.179"
+IP1="140.238.3.232"
+IP2="158.101.103.98"
+IP3="129.213.164.217"
 
 tendermint node --home ./mytestnet/node$1 --proxy_app=kvstore --p2p.persistent_peers=$ID0@$IP0:26656,$ID1@$IP1:26656,$ID2@$IP2:26656,$ID3@$IP3:26656
